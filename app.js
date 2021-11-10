b07a42fd7840c6cad94e94b0624e71738c415bf9
nicomellon <nmellon94@gmail.com>
2021-11-10 10:24:26 +0100

    fix static path

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require("path");
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, "/views")));
 
 app.all("/", (request, response) => {
   response.sendFile(path.join(__dirname, "views", "index.html"));
